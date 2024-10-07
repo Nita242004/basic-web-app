e87bbdde767fa6cb8b98274f94edd0c66992dea4
Kanittha Thaveesittikullarp <nita242004@gmail.com>
2024-10-07 13:24:48 -0400

    Edit

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("What is your Andrew ID?")) {
-    return "andrew_id"; 
+    return "your_andrew_id"; 
   }
 
   return "";
